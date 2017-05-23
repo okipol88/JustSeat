49cbd54a61c1ca56002ab45313c93245f8accefa
Błażej Szajrych <blazej.szajrych@gmail.com>
2017-05-24 00:32:44 +0200

    Added [pssobo;oty tp add new table

--- JustSeat/ViewModel/MainViewModel.cs
+++ JustSeat/ViewModel/MainViewModel.cs
@@ -7,6 +7,7 @@ using JustSeat.ViewModel.Handlers;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Input;
+using System;
 
 namespace JustSeat.ViewModel
 {
@@ -24,15 +25,15 @@ namespace JustSeat.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        int side = 80;
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
+            var posMultiplier = 90;
             //if (IsInDesignMode)
             {
-                var side = 80;
-                var posMultiplier = 90;
 
                 Enumerable.Range(0, 5).ToList().ForEach(i =>
                  {
@@ -42,45 +43,63 @@ namespace JustSeat.ViewModel
                  });
 
                 ((Table)Items[0]).TopChairs[0].Person = Guests.First();
-
-                RemoveGuestCommand = new RelayCommand<Chair>(
-                (c) =>
-                {
-                    var person = c.Person;
-                    if (c == null || person == null)
-                        return;
-                    c.Person = null;
-
-                    Guests.Add(person);
-                },
-                (c) =>
-                {
-                    if (c == null)
-                        return false;
-
-                    return c.Person != null;
-                });
-
-                RemovePersonOrChairCommand = new RelayCommand<ChairRemovalParameter>((param) =>
-                  {
-                      if (param == null)
-                          return;
-
-                      if (param.ChairToRemove.Person != null)
-                      {
-                          Guests.Add(param.ChairToRemove.Person);
-                          param.ChairToRemove.Person = null;
-                      }
-                      else
-                      {
-                          param.Table.TopChairs.Remove(param.ChairToRemove);
-                          param.Table.BottomChairs.Remove(param.ChairToRemove);
-                          param.Table.LeftChairs.Remove(param.ChairToRemove);
-                          param.Table.RightChairs.Remove(param.ChairToRemove);
-                      }
-
-                  });
             }
+
+            RemoveGuestCommand = new RelayCommand<Chair>(
+            (c) =>
+            {
+                var person = c.Person;
+                if (c == null || person == null)
+                    return;
+                c.Person = null;
+
+                Guests.Add(person);
+            },
+            (c) =>
+            {
+                if (c == null)
+                    return false;
+
+                return c.Person != null;
+            });
+
+            RemovePersonOrChairCommand = new RelayCommand<ChairRemovalParameter>((param) =>
+            {
+                if (param == null)
+                    return;
+
+                if (param.ChairToRemove.Person != null)
+                {
+                    Guests.Add(param.ChairToRemove.Person);
+                    param.ChairToRemove.Person = null;
+                }
+                else
+                {
+                    param.Table.TopChairs.Remove(param.ChairToRemove);
+                    param.Table.BottomChairs.Remove(param.ChairToRemove);
+                    param.Table.LeftChairs.Remove(param.ChairToRemove);
+                    param.Table.RightChairs.Remove(param.ChairToRemove);
+                }
+
+            });
+
+            AddTableCommand = new RelayCommand(() =>
+            {
+                AddNewTable();
+            });
+
+            RemoveTableCommand = new RelayCommand<Table>((table) =>
+            {
+                var items = table.LeftChairs.ToList();
+                items.AddRange(table.TopChairs);
+                items.AddRange(table.BottomChairs);
+                items.AddRange(table.RightChairs);
+
+                items.Where(x => x.Person != null).Select(x => x.Person)
+                .ToList().ForEach(p => Guests.Add(p));
+
+                Items.Remove(table);
+            });
 
             var dropHandler = new GuestToSeatDropHandler();
             dropHandler.GuestDropped += (o, e) =>
@@ -91,8 +110,13 @@ namespace JustSeat.ViewModel
             GuestOnChairDropHandler = dropHandler;
         }
 
-        public ObservableCollection<Guest> Guests { get; set; } = new ObservableCollection<Guest>();
-        public ObservableCollection<ICanvasDisplayItem> Items { get; set; } = new ObservableCollection<ICanvasDisplayItem>();
+        private void AddNewTable()
+        {
+            Items.Add(new Table() { Width = side, Length = side });
+        }
+
+        public ObservableCollection<Guest> Guests { get; } = new ObservableCollection<Guest>();
+        public ObservableCollection<ICanvasDisplayItem> Items { get; } = new ObservableCollection<ICanvasDisplayItem>();
 
         public IDropTarget GuestOnChairDropHandler { get; private set; }
 
@@ -103,8 +127,8 @@ namespace JustSeat.ViewModel
         public RelayCommand<Table> AddLeftChairCommand { get; } = new RelayCommand<Table>((table) => { table.LeftChairs.Add(new Chair()); });
         public RelayCommand<Table> AddRightChairCommand { get; } = new RelayCommand<Table>((table) => { table.RightChairs.Add(new Chair()); });
 
-
         public RelayCommand<ChairRemovalParameter> RemovePersonOrChairCommand { get; private set; }
-
+        public RelayCommand AddTableCommand { get; private set; }
+        public RelayCommand<Table> RemoveTableCommand { get; private set; }
     }
 }
