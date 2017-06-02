a1d6af610733ad3af6a9ffcf3daa012e3e5430a6
Błażej Szajrych <blazej.szajrych@gmail.com>
2017-06-02 20:49:21 +0200

    Added slide in panel

---
+++ JustSeat/Converter/ToggleBooleanValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace JustSeat.Converter
+{
+    public class ToggleBooleanValueConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = value as bool?;
+            if (!val.HasValue)
+                return null;
+
+            return !val.Value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
